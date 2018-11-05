ebc98d7792a723e43db7d4d8783f78a9d24d8e1c
Maksim Vigura <mvigura@gmail.com>
2018-11-05 23:45:26 +0500

    sort function first ver

--- src/app/item.service.ts
+++ src/app/item.service.ts
@@ -27,4 +27,19 @@ export class ItemService {
       this.items.splice(index, 1);
     }
   }
+
+  sortItems(sortSettings: {type: string; date: string}) {
+    this.items.sort((item_1: Item, item_2: Item) => {
+      const date_1 = new Date(item_1.date).getTime();
+      const date_2 = new Date(item_2.date).getTime();
+      // return sortSettings.date === 'asc' ? date_1 - date_2 : date_2 - date_1;
+      console.log(Object.getPrototypeOf(item_1).constructor.name);
+      const prot_1 = Object.getPrototypeOf(item_1).constructor.name;
+      const prot_2 = Object.getPrototypeOf(item_2).constructor.name;
+      if (prot_1 > prot_2) return -1;
+      if (prot_1 < prot_2) return 1;
+      return 0;
+      // return date_1 - date_2;
+    });
+  }
 }
