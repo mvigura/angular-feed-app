71ec01dd8c473770adf867db6b37817e1be6fcfb
Maksim Vigura <mvigura@gmail.com>
2018-11-05 23:43:04 +0500

    add news form

--- src/app/forms/add-news-form/add-news-form.component.ts
+++ src/app/forms/add-news-form/add-news-form.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormBuilder} from '@angular/forms';
+import {News} from 'src/app/_classes';
+import {ModalService} from 'src/app/modal.service';
+import {ItemService} from 'src/app/item.service';
 
 @Component({
   selector: 'app-add-news-form',
   templateUrl: './add-news-form.component.html',
   styleUrls: ['./add-news-form.component.less']
 })
-export class AddNewsFormComponent implements OnInit {
+export class AddNewsFormComponent {
+  constructor(
+    private fb: FormBuilder,
+    private modalService: ModalService,
+    private itemService: ItemService
+  ) {}
 
-  constructor() { }
+  addNewsForm = this.fb.group({
+    title: '',
+    content: ''
+  });
 
-  ngOnInit() {
+  onSubmit(values) {
+    const newNews = new News('dsadsda', Date().toString(), values.title, values.content, false);
+    this.itemService.addItem(newNews);
+    this.modalService.close('add-event');
   }
-
 }
