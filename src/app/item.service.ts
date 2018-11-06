2be028b96d5a3a3f5e32638ec3cbb6f084a4becd
Maksim Vigura <mvigura@gmail.com>
2018-11-06 16:48:25 +0500

    sort finalize

--- src/app/item.service.ts
+++ src/app/item.service.ts
@@ -11,14 +11,19 @@ export class ItemService {
   constructor() {}
 
   items: Item[] = [];
+  sortSettings: {type: string; date: string} = {type: 'desc', date: 'desc'};
 
   getItems(): Observable<Item[]> {
     this.items = [...TRANSACTIONS, ...NEWS];
+    this.sortByType();
+    this.sortByDate();
     return of(this.items);
   }
 
   addItem(item: Item) {
     this.items.push(item);
+    this.sortByType();
+    this.sortByDate();
   }
 
   deleteItem(item: Item) {
@@ -28,18 +33,42 @@ export class ItemService {
     }
   }
 
-  sortItems(sortSettings: {type: string; date: string}) {
+  sortByType() {
+    const sortDirection = this.sortSettings.type;
+    this.items.sort((item_1: Item, item_2: Item) => {
+      const prot_1 = Object.getPrototypeOf(item_1).constructor.name;
+      const prot_2 = Object.getPrototypeOf(item_2).constructor.name;
+
+      if (sortDirection === 'asc') {
+        if (prot_1 < prot_2) return -1;
+        if (prot_1 > prot_2) return 1;
+      } else {
+        if (prot_1 > prot_2) return -1;
+        if (prot_1 < prot_2) return 1;
+      }
+      return 0;
+    });
+  }
+
+  sortByDate() {
+    const sortDirection = this.sortSettings.date;
     this.items.sort((item_1: Item, item_2: Item) => {
       const date_1 = new Date(item_1.date).getTime();
       const date_2 = new Date(item_2.date).getTime();
-      // return sortSettings.date === 'asc' ? date_1 - date_2 : date_2 - date_1;
-      console.log(Object.getPrototypeOf(item_1).constructor.name);
-      const prot_1 = Object.getPrototypeOf(item_1).constructor.name;
-      const prot_2 = Object.getPrototypeOf(item_2).constructor.name;
-      if (prot_1 > prot_2) return -1;
-      if (prot_1 < prot_2) return 1;
-      return 0;
-      // return date_1 - date_2;
+      return sortDirection === 'asc' ? date_1 - date_2 : date_2 - date_1;
     });
   }
+
+  setSort(sortType: string) {
+    this.sortSettings[sortType] = this.sortSettings[sortType] === 'asc' ? 'desc' : 'asc';
+
+    if (sortType === 'type') {
+      this.sortByType();
+    } else {
+      this.sortByDate();
+    }
+  }
+  // sortItems(sortSettings: {type: string; date: string}) {
+  //   this.items.sort(this.sortByDate(sortSettings.date));
+  // }
 }
--- src/app/subheader/subheader.component.ts
+++ src/app/subheader/subheader.component.ts
@@ -9,7 +9,7 @@ import {ItemService} from '../item.service';
 export class SubheaderComponent implements OnInit {
   constructor(private itemService: ItemService) {}
 
-  sortSettings: {type: string; date: string} = {type: 'asc', date: 'asc'};
+  sortSettings = this.itemService.sortSettings;
 
   ngOnInit() {}
 
@@ -18,9 +18,6 @@ export class SubheaderComponent implements OnInit {
   }
 
   setSort(sortType: string) {
-    this.sortSettings[sortType] === 'asc'
-      ? (this.sortSettings[sortType] = 'desc')
-      : (this.sortSettings[sortType] = 'asc');
-    this.itemService.sortItems(this.sortSettings);
+    this.itemService.setSort(sortType);
   }
 }
